d31bef6e527879ae8d1b7776c7370264b4382913
Alfonso Fidalgo <afidalgo@tripadvisor.com>
2017-02-27 22:18:15 +0000

    Added create and edit routes

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const app = express();
 const bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/muber');
+if (process.env.NODE_ENV !== 'test'){
+  mongoose.connect('mongodb://localhost/muber');
+}
 
 app.use(bodyParser.json()); //before routes!!
 routes(app);
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message});
+});
 
 
 module.exports = app;
